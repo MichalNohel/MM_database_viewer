014903c3a68f8186d419cab13c9d14e7d9bc0a76
Michal Nohel <nohel@PC-SE4158-779.ubmi.feec.vutb.cz>
2024-07-19 15:03:06 +0200

    update

--- Database_viewer.py
+++ Database_viewer.py
@@ -36,16 +36,22 @@ if __name__ == "__main__":
     # Načtení všech DICOM souborů z adresáře, vynechání souboru DIRFILE
     dicom_files = [os.path.join(dicom_dir, f) for f in os.listdir(dicom_dir) if f != 'DIRFILE']
     
+    series_description = pydicom.dcmread(dicom_files[0]).get('SeriesDescription')
+    
     # Načtení metadat a pixelových dat
     slices = [pydicom.dcmread(dicom_file) for dicom_file in dicom_files]
     
     # Seřazení slices podle SliceLocation nebo InstanceNumber
     slices.sort(key=lambda x: float(x.ImagePositionPatient[2]))
     
+    
+    #%% 
     # Extrahování pixelových dat
-    image_data_txz = np.stack([s.pixel_array for s in slices])
+    image_data_zxy = np.stack([s.pixel_array for s in slices])
     
-    
+    image_data_tzxy=np.zeros([2,1095,512,512])
+    image_data_tzxy[0,:,:,:]=image_data_zxy
+    image_data_tzxy[1,:,:,:]=image_data_zxy
     # viewer = napari.view_image(image_data)
     # napari.run()
     
@@ -60,28 +66,31 @@ if __name__ == "__main__":
     
     #SegmMaskSpine = sitk.GetArrayFromImage(sitk.ReadImage(path_spine_mask)).astype(np.float32)
     SegmMaskSpine = nib.load(path_spine_mask[0]).get_fdata()
-    SegmMaskSpine_txy=SegmMaskSpine.transpose(2,0,1)
-    SegmMaskSpine_txy = np.rot90(SegmMaskSpine_txy, k=1, axes=(1, 2))
+    SegmMaskSpine_zxy=SegmMaskSpine.transpose(2,0,1)
+    SegmMaskSpine_zxy = np.rot90(SegmMaskSpine_zxy, k=1, axes=(1, 2))
 
     
     #SegmMaskLesions = sitk.GetArrayFromImage(sitk.ReadImage(path_lesion_mask)).astype(np.float32)
     SegmMaskLesions = nib.load(path_lesion_mask[0]).get_fdata()
-    SegmMaskLesions_txy=SegmMaskLesions.transpose(2,0,1)
-    SegmMaskLesions_txy = np.rot90(SegmMaskLesions_txy, k=1, axes=(1, 2))
+    SegmMaskLesions_zxy=SegmMaskLesions.transpose(2,0,1)
+    SegmMaskLesions_zxy = np.rot90(SegmMaskLesions_zxy, k=1, axes=(1, 2))
     #%%
     v = napari.Viewer()
-    datalayer = v.add_image(image_data_txz, name='data')    
+    #datalayer = v.add_image(image_data_zxy, name='data')    
+    datalayer = v.add_image(image_data_tzxy, name='data')   
+    
     datalayer.colormap = 'gray'
     datalayer.blending = 'additive'
-    
-    
-    SpineMaskLayer = v.add_image(SegmMaskSpine_txy, name='SpineMask')
-    SpineMaskLayer.colormap = 'gray'
+        
+    SpineMaskLayer = v.add_image(SegmMaskSpine_zxy, name='SpineMask')
+    SpineMaskLayer.colormap = 'blue'
     SpineMaskLayer.blending = 'additive'
+    SpineMaskLayer.opacity = 0.5
     
-    SpineMaskLayer = v.add_image(SegmMaskLesions_txy, name='LessionMask')
-    SpineMaskLayer.colormap = 'red'
-    SpineMaskLayer.blending = 'additive'
+    LesionMaskLayer = v.add_image(SegmMaskLesions_zxy, name='LessionMask')
+    LesionMaskLayer.colormap = 'red'
+    LesionMaskLayer.blending = 'additive'
+    LesionMaskLayer.opacity = 1
     
     
     napari.run()
