ab3e86d7194e81e1576107c85457dde462358a4f
Michal Nohel <nohel@PC-SE4158-779.ubmi.feec.vutb.cz>
2024-07-22 12:14:17 +0200

    update

--- Database_viewer.py
+++ Database_viewer.py
@@ -11,11 +11,79 @@ import pydicom
 import nibabel as nib
 import numpy as np
 import os
+from nibabel.orientations import axcodes2ornt, ornt_transform, apply_orientation
+
 join=os.path.join
 
+def get_dicom_orientation(dicom_file_path):
+    # Načtení DICOM souboru
+    dicom_data = pydicom.dcmread(dicom_file_path)
+    
+    # Získání ImageOrientationPatient a ImagePositionPatient
+    image_orientation = dicom_data.ImageOrientationPatient
+    
+    # Výpis hodnot
+    print(f"ImageOrientationPatient: {image_orientation}")
+    
+    # Převod na numpy pole pro snadnější manipulaci
+    image_orientation = np.array(image_orientation).reshape(2, 3)
+    
+    # Definice os
+    row_cosines = image_orientation[0, :]
+    col_cosines = image_orientation[1, :]
+    
+    # Výpočet cross product pro získání normálového vektoru k obrazové rovině
+    normal_cosines = np.cross(row_cosines, col_cosines)
+    
+    # Sloučení do jedné matice (řádek, sloupec, normála)
+    orientation_matrix = np.vstack((row_cosines, col_cosines, normal_cosines))
+    
+    # Definice os podle orientace
+    axis_labels = ['R', 'A', 'S']
+    
+    orientation = []
+    for axis in orientation_matrix:
+        max_index = np.argmax(np.abs(axis))
+        sign = np.sign(axis[max_index])
+        orientation.append(axis_labels[max_index] if sign > 0 else axis_labels[max_index].swapcase())
+    
+    return ''.join(orientation)
 
+def reorient_nifti_to_ras(nifti_file_path):
+    # Načtení NIFTI souboru
+    nifti_img = nib.load(nifti_file_path)
+    nifti_data = nifti_img.get_fdata()
+    nifti_affine = nifti_img.affine
 
+    # Zjištění aktuální orientace
+    current_orientation = nib.aff2axcodes(nifti_affine)
+    print(f"Current orientation: {current_orientation}")
+
+    # Cílová orientace (RAS)
+    target_orientation = ('R', 'A', 'S')
+    
+    # Transformace orientace
+    ornt_trans = ornt_transform(axcodes2ornt(current_orientation), axcodes2ornt(target_orientation))
+    reoriented_data = apply_orientation(nifti_data, ornt_trans)
+
+    # Vytvoření nové afinní matice po reorientaci
+    reoriented_affine = nifti_affine @ nib.orientations.inv_ornt_aff(ornt_trans, nifti_data.shape)
+    
+    # Vytvoření nového NIFTI obrazu s reorientovanými daty a aktualizovanou afinní maticí
+    reoriented_nifti_img = nib.Nifti1Image(reoriented_data, reoriented_affine)
+
+    return reoriented_nifti_img
+
+#%%
 if __name__ == "__main__":
+    
+    #%%
+    dicom_file_path = 'F:/MM_Dataset/S80060/S207300/I10'
+    orientation = get_dicom_orientation(dicom_file_path)
+    print(f"DICOM Orientation: {orientation}")
+    
+    
+    #%%
     
     base='../MM_Dataset/'
     ID_patient="S80060"
@@ -45,6 +113,7 @@ if __name__ == "__main__":
     slices.sort(key=lambda x: float(x.ImagePositionPatient[2]))
     
     
+    
     #%% 
     # Extrahování pixelových dat
     image_data_zxy = np.stack([s.pixel_array for s in slices])
@@ -55,24 +124,30 @@ if __name__ == "__main__":
     # viewer = napari.view_image(image_data)
     # napari.run()
     
-    
+    # %%
     #Load Masks
     path_spine_mask= [os.path.join(patient_main_file, f) for f in os.listdir(patient_main_file) if f.endswith('nnUNet_cor.nii.gz')]
     
     path_lesion_mask= [os.path.join(patient_main_file, f) for f in os.listdir(patient_main_file) if f.endswith('final.nii.gz')]
     
-    
+    #%%
     #sitk.ProcessObject_SetGlobalWarningDisplay(False)
     
-    #SegmMaskSpine = sitk.GetArrayFromImage(sitk.ReadImage(path_spine_mask)).astype(np.float32)
-    SegmMaskSpine = nib.load(path_spine_mask[0]).get_fdata()
-    SegmMaskSpine_zxy=SegmMaskSpine.transpose(2,0,1)
-    SegmMaskSpine_zxy = np.rot90(SegmMaskSpine_zxy, k=1, axes=(1, 2))
-
+    # SegmMaskSpine_tzxy = sitk.GetArrayFromImage(sitk.ReadImage(path_spine_mask)).astype(np.float32)
+    # SegmMaskSpine_zxy=SegmMaskSpine_tzxy[0,:,:,:]
     
-    #SegmMaskLesions = sitk.GetArrayFromImage(sitk.ReadImage(path_lesion_mask)).astype(np.float32)
+    # SegmMaskLesions_tzxy = sitk.GetArrayFromImage(sitk.ReadImage(path_lesion_mask)).astype(np.float32)
+    # SegmMaskLesions_zxy=SegmMaskLesions_tzxy[0,:,:,:]
+    
+    
+    #%%
+    SegmMaskSpine = nib.load(path_spine_mask[0]).get_fdata()
+    SegmMaskSpine_zxy = SegmMaskSpine.transpose(2,0,1)
+    SegmMaskSpine_zxy = np.rot90(SegmMaskSpine_zxy, k=1, axes=(1, 2))
+    
+
     SegmMaskLesions = nib.load(path_lesion_mask[0]).get_fdata()
-    SegmMaskLesions_zxy=SegmMaskLesions.transpose(2,0,1)
+    SegmMaskLesions_zxy = SegmMaskLesions.transpose(2,0,1)
     SegmMaskLesions_zxy = np.rot90(SegmMaskLesions_zxy, k=1, axes=(1, 2))
     #%%
     v = napari.Viewer()
